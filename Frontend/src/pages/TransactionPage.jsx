ba98730727adbf0bb5ab81bd5039db84de86fc00
ameni.grami <amenigrami@takiacademyteam.com>
2024-03-20 15:50:53 +0100

    implement edit transaction

--- Frontend/src/pages/TransactionPage.jsx
+++ Frontend/src/pages/TransactionPage.jsx
@@ -1,20 +1,57 @@
-import { useState } from "react";
-import TransactionFormSkeleton from "../components/skeletons/sectionFormSkeleton";
+import { useMutation, useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+    GET_TRANSACTION,
+    GET_TRANSACTION_STATISTICS,
+} from "../graphql/queries/transaction.query";
+import { UPDATE_TRANSACTION } from "../graphql/mutations/transcation.mutation";
+import toast from "react-hot-toast";
+import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 
 const TransactionPage = () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_TRANSACTION, {
+        variables: { id: id },
+    });
+
+    console.log("Transaction", data);
+
+    const [updateTransaction, { loading: loadingUpdate }] = useMutation(
+        UPDATE_TRANSACTION,
+        {
+            refetchQueries: [{ query: GET_TRANSACTION_STATISTICS }],
+        },
+    );
+
     const [formData, setFormData] = useState({
-        description: "",
-        paymentType: "",
-        category: "",
-        amount: "",
-        location: "",
-        date: "",
+        description: data?.transaction?.description || "",
+        paymentType: data?.transaction?.paymentType || "",
+        category: data?.transaction?.category || "",
+        amount: data?.transaction?.amount || "",
+        location: data?.transaction?.location || "",
+        date: data?.transaction?.date || "",
     });
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("formData", formData);
+        const amount = parseFloat(formData.amount);
+        try {
+            await updateTransaction({
+                variables: {
+                    input: {
+                        ...formData,
+                        amount,
+                        transactionId: id,
+                    },
+                },
+            });
+            toast.success("Transaction updated successfully");
+        } catch (error) {
+            toast.error(error.message);
+        }
     };
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevFormData) => ({
@@ -22,7 +59,22 @@ const TransactionPage = () => {
             [name]: value,
         }));
     };
-    const loading = false;
+
+    useEffect(() => {
+        if (data) {
+            setFormData({
+                description: data?.transaction?.description,
+                paymentType: data?.transaction?.paymentType,
+                category: data?.transaction?.category,
+                amount: data?.transaction?.amount,
+                location: data?.transaction?.location,
+                date: new Date(+data.transaction.date)
+                    .toISOString()
+                    .substr(0, 10),
+            });
+        }
+    }, [data]);
+
     if (loading) return <TransactionFormSkeleton />;
 
     return (
@@ -138,7 +190,6 @@ const TransactionPage = () => {
                     </div>
                 </div>
 
-                {/* LOCATION */}
                 <div className="flex flex-wrap gap-3">
                     <div className="w-full flex-1 mb-6 md:mb-0">
                         <label
@@ -178,13 +229,14 @@ const TransactionPage = () => {
                         />
                     </div>
                 </div>
-                {/* SUBMIT BUTTON */}
+
                 <button
                     className="text-white font-bold w-full rounded px-4 py-2 bg-gradient-to-br
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600"
                     type="submit"
+                    disabled={loadingUpdate}
                 >
-                    Update Transaction
+                    {loadingUpdate ? "Updating..." : "Update Transaction"}
                 </button>
             </form>
         </div>
