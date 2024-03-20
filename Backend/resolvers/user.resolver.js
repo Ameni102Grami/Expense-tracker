8d9912ab207cbb49f21cb79aecda4de57be3b03e
ameni.grami <amenigrami@takiacademyteam.com>
2024-03-20 09:17:17 +0100

    configure passport

--- Backend/resolvers/user.resolver.js
+++ Backend/resolvers/user.resolver.js
@@ -2,9 +2,10 @@ import { users } from "./../dummyData/data.js";
 
 const userResolver = {
     Query: {
-        users: () => {
+        users: (_, t, { req, res }) => {
             return users;
         },
+
         user: (_, { userId }) => {
             return users.find((el) => el._id == userId);
         },
