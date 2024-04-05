6ba8f734c3efea33702ee1d9fe96039ba27fbeac
ameni.grami <amenigrami@takiacademyteam.com>
2024-04-05 13:49:00 +0100

    fixing the logic of the expenses calculation

--- Backend/notification-service/controllers.js
+++ Backend/notification-service/controllers.js
@@ -1,9 +1,10 @@
 import KafkaConfig from "./config.js";
+import { calculateExpenseNotification } from "./utils/CalculateExpenses.js";
 
 const sendMessageToKafka = async (req, res) => {
   try {
     const { message } = req.body;
-    
+
     let totalExpenses = 0;
 
     message?.categoryData.forEach((item) => {
@@ -12,40 +13,16 @@ const sendMessageToKafka = async (req, res) => {
       }
     });
     const kafkaConfig = new KafkaConfig();
-    let notification;
 
-    if (Number(message.user.budget) * 0.9 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering 90% or nearly 90% of your budget. Please be cautious.";
-    } else if (Number(message.user.budget) * 0.8 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 80% of your budget. Consider reviewing your spending.";
-    } else if (Number(message.user.budget) * 0.7 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 70% of your budget. It's time to reassess your spending habits.";
-    } else if (Number(message.user.budget) * 0.6 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 60% of your budget. You might want to cut down on unnecessary expenses.";
-    } else if (Number(message.user.budget) * 0.5 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 50% of your budget. Take immediate action to reduce expenses.";
-    } else if (Number(message.user.budget) * 0.4 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 40% of your budget. You are spending too much. Take control now!";
-    } else if (Number(message.user.budget) * 0.3 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 30% of your budget. You are on the verge of financial crisis. Act now!";
-    } else if (Number(message.user.budget) * 0.2 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 20% of your budget. Serious financial trouble ahead! Save every penny.";
-    } else if (Number(message.user.budget) * 0.1 >= Number(totalExpenses)) {
-      notification =
-        "Your expenses are covering more than 10% of your budget. Dangerously close to bankruptcy. Save your last dime!";
-    } else {
-      notification =
-        "Your expenses have exceeded your budget. Take immediate steps to control your spending.";
-    }
-    const messages = [{ key: "key1", value: notification }];
+    const messages = [
+      {
+        key: "key1",
+        value: calculateExpenseNotification(
+          Number(message.user.budget),
+          Number(totalExpenses)
+        ),
+      },
+    ];
 
     kafkaConfig.produce("my-topic", messages);
 
---
+++ Backend/notification-service/utils/CalculateExpenses.js
@@ -0,0 +1,27 @@
+export function calculateExpenseNotification(budget, totalExpenses) {
+  const expensePercentage = totalExpenses / (budget * 100); // Multiply by 100 to get the percentage
+
+  if (expensePercentage >= 100) {
+    return "Your expenses have exceeded your budget. Take immediate steps to control your spending.";
+  } else if (expensePercentage >= 90) {
+    return "Your expenses are covering 90% or nearly 90% of your budget. Please be cautious.";
+  } else if (expensePercentage >= 80) {
+    return "Your expenses are covering more than 80% of your budget. Consider reviewing your spending.";
+  } else if (expensePercentage >= 70) {
+    return "Your expenses are covering more than 70% of your budget. It's time to reassess your spending habits.";
+  } else if (expensePercentage >= 60) {
+    return "Your expenses are covering more than 60% of your budget. You might want to cut down on unnecessary expenses.";
+  } else if (expensePercentage >= 50) {
+    return "Your expenses are covering more than 50% of your budget. Take immediate action to reduce expenses.";
+  } else if (expensePercentage >= 40) {
+    return "Your expenses are covering more than 40% of your budget. You are spending too much. Take control now!";
+  } else if (expensePercentage >= 30) {
+    return "Your expenses are covering more than 30% of your budget. You are on the verge of financial crisis. Act now!";
+  } else if (expensePercentage >= 20) {
+    return "Your expenses are covering more than 20% of your budget. Serious financial trouble ahead! Save every penny.";
+  } else if (expensePercentage >= 10) {
+    return "Your expenses are covering more than 10% of your budget. Dangerously close to bankruptcy. Save your last dime!";
+  } else {
+    return "Your expenses are within 10% of your budget. You're managing well, keep it up!";
+  }
+}
